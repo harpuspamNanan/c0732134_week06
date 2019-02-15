1ade9865294757ffeb869f3c12cb16e4e3b004d7
hps3103 <ssingh.harpushpam@gmailc.om>
2019-02-15 18:05:12 -0500

    Start working on the Assignment

--- c0732134/Program.cs
+++ c0732134/Program.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace c0732134
 {
@@ -15,53 +11,6 @@ namespace c0732134
         }
     }
 
-    class UsingRecursion
-    {
-        // Create an application which 
-        // calculates the sum of all the numbers from n to m recursively:
-
-        public static int CalculateRecursively(int n, int m)
-        {
-            int sum = n;
-
-            if (n < m)
-            {
-                n++;
-                return sum += CalculateRecursively(n, m);
-            }
-            return sum;
-        }
-
-
-    }
-
-    class CountrySide
-    {
-        // Create the LinkedList to reflect the Map in the PowerPoint Instructions
-        Village Maeland;
-        Village Helmholtz;
-        Village Alst;
-        Village Wessig;
-        Village Badden;
-        Village Uster; 
-        Village Schvenig;
-
-
-        class Village
-        {
-            public Village(string _villageName, bool _isAHere)
-            {
-                isAstrildgeHere = _isAHere;
-                VillageName = _villageName;
-            }
-
-            public Village west;
-            public Village east;
-            public string VillageName;
-            public int distanceToNextVillage;
-            public int distanceToPreviousVillage;
-            public bool isAstrildgeHere;
-        }
-    }
+   
 
 }
